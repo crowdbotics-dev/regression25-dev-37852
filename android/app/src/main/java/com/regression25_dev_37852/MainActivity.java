b34609a17f299d9379185ccbf543347665a6e60c
Crowdbotics <engineering@crowdbotics.com>
2022-10-10 09:50:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/regression25_dev_37852/MainActivity.java
@@ -0,0 +1,15 @@
+package com.regression25_dev_37852;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "regression25_dev_37852";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "regression25_dev_37852",
+  url: "https://regression25_dev_37852.botics.co",
+  api: "https://regression25_dev_37852.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
