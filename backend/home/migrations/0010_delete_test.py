8f93bcab1e6f40e195fd131d08fe93f1fead889c
Crowdbotics <engineering@crowdbotics.com>
2022-10-12 17:45:40 +0000

    Update models

--- backend/home/migrations/0010_delete_test.py
+++ backend/home/migrations/0010_delete_test.py
@@ -6,11 +6,11 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0009_delete_himanshutest'),
+        ("home", "0009_delete_himanshutest"),
     ]
 
     operations = [
         migrations.DeleteModel(
-            name='Test',
+            name="Test",
         ),
     ]
